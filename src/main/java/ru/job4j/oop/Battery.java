39e5f1de3747437935c8723c27bfe84a1aeff8a2
Adel Dubinnikova <a.dubinnikova@carprice.tech>
2021-01-04 07:43:30 +0300

    1.7. Состояние объекта [#420933]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int num) {
+        this.load = num;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery one = new Battery(20);
+        Battery two = new Battery(43);
+        System.out.println("Battery one contains " + one.load);
+        System.out.println("Battery two contains " + two.load);
+        one.exchange(two);
+        System.out.println("Now battery one contains " + one.load);
+        System.out.println("Now battery two contains " + two.load);
+    }
+}

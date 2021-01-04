f504ffc3eb4097aa53d8e486735c64879348b685
Adel Dubinnikova <a.dubinnikova@carprice.tech>
2021-01-05 00:34:38 +0300

    2. Перегрузка конструктора [#420939]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,20 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {}
+
+    public Item(int id) {
+    }
+
+    public Item(int id, String name) {
+    }
+
+    public static void main(String[] args) {
+        Item newTest = new Item();
+        Item secondTest = new Item(2);
+        Item thirdTest = new Item(3, "TestName");
+    }
+
     public int getId() {
         return id;
     }
